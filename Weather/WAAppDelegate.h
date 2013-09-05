da3724a4ba360a08cc948b91fd7ac297975b4101
Siyana Slavova <siyana.slavova@Student04s-Mac-mini.local>
2013-09-05 16:15:32 +0300

    Initial Commit

---
+++ Weather/WAAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  WAAppDelegate.h
+//  Weather
+//
+//  Created by Siyana Slavova on 9/5/13.
+//  Copyright (c) 2013 Siyana Slavova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Weather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Weather
+//
+//  Created by Siyana Slavova on 9/5/13.
+//  Copyright (c) 2013 Siyana Slavova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WAAppDelegate class]));
+    }
+}
