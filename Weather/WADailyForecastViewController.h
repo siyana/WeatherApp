1d0314b2b350e39fa6b4ac0aa33aaf4f60737b01
Siyana Slavova <siyana.slavova@mentormate.com>
2013-09-11 20:07:36 +0300

    no message

---
+++ Weather/WADailyForecastViewController.h
@@ -0,0 +1,13 @@
+//
+//  WADailyForecastViewController.h
+//  Weather
+//
+//  Created by Siyana Slavova on 9/11/13.
+//  Copyright (c) 2013 Siyana Slavova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WADailyForecastViewController : UITableViewController
+
+@end
